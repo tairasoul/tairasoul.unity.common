91324c6a23e677a16102e318161da4a59b1ce288
tairasoul <100938045+tairasoul@users.noreply.github.com>
2025-12-21 15:44:09 +0200

    oh thats why it wasnt working

--- embedded/dependency_loader.cs
+++ embedded/dependency_loader.cs
@@ -9,13 +9,18 @@ static class EmbeddedDependencyLoader {
 	public static void Init(AppDomain domain, string prefix, string[] assemblyNames) {
 		domain.AssemblyResolve += (context, name) =>
 		{
-			if (assemblyNames.Contains(name.Name)) {
-				AssemblyName aname = new(name.Name);
-				var primary = Assembly.Load(aname);
-				if (primary != null) return primary;
-				var bytes = AssemblyUtils.GetResourceBytes($"{prefix}.{aname.Name}.dll");
-				if (bytes.Length <= 0) return null;
-				return Assembly.Load(bytes);
+			AssemblyName aname = new(name.Name);
+			if (assemblyNames.Contains(aname.Name)) {
+				try
+				{
+					var primary = Assembly.Load(aname);
+					if (primary != null) return primary;
+				}
+				catch {
+					var bytes = AssemblyUtils.GetResourceBytes($"{prefix}.{aname.Name}.dll");
+					if (bytes.Length <= 0) return null;
+					return Assembly.Load(bytes);
+				}
 			}
 			return null;
 		};
