785c917cff5589d4b419352a0c28f783e623c33a
tairasoul <100938045+tairasoul@users.noreply.github.com>
2025-12-28 13:19:51 +0200

    note that this is unimplemented

--- speedrunning/dsl/compiler/analysis.cs
+++ speedrunning/dsl/compiler/analysis.cs
@@ -2,6 +2,8 @@ namespace tairasoul.unity.common.speedrunning.dsl.compiler;
 
 abstract record SemanticAnalysisResult();
 
+// unimplemented semantic analysis visitor
+
 class SemanticAnalysis : SrDslParserBaseVisitor<SemanticAnalysisResult> {
 
 }
